a8d715d520daabc1113837a40d5f41069ee74c96
Jon Balon <development@jonbalon.com>
2016-08-28 21:59:59 -0400

    Added styles and content to <AboutMe/>

--- src/js/UserInterface/sections/AboutMe.jsx
+++ src/js/UserInterface/sections/AboutMe.jsx
@@ -13,7 +13,30 @@ export default class AboutMe extends React.Component {
   render() {
     return (
       <div className="section" id="section-aboutme">
-        AboutMe
+
+        <div className="title">About Me</div>
+
+        <div className="blurb text-center">
+
+          <p>
+            Hi! My name is Jon and I'm a Front-end Web Developer based in
+            Toronto.
+          </p>
+
+          <p>
+            For the past year I have been focusing on building responsive
+            websites to complement various back-end technologies such as Debian
+             Linux, Python, Flask, and MongoDB.
+          </p>
+
+          <p>
+            I am currently in the process of completing the Full Stack Developer
+             Certification on FreeCodeCamp.com and learning advanced libraries
+             and frameworks such as React.js and AngularJS 2.0.
+          </p>
+
+        </div>
+
       </div>
     );
   } // End Component Render.
