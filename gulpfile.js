2e986c5bb644c6156e5b46188d8f1dfefa6ba7e5
Jon Balon <development@jonbalon.com>
2016-08-29 02:14:50 -0400

    Remove flexboxgrid, gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ var gulp = require('gulp');             // Gulp.
 var connect = require('gulp-connect');  // Webserver.
 var sass = require('gulp-sass');        // SASS.
 var webpack = require('webpack-stream');// Webpack.
-var rename = require('gulp-rename');    // Rename files.
 
 ////////////////////////////////////////////////////////////////////////////////
 // Paths
@@ -49,12 +48,6 @@ gulp.task('media', function() {
 
 // Compile .scss and move.
 gulp.task('stylesheets', function() {
-
-  // Get flexboxgrid.
-  gulp.src(modulesPath + 'flexboxgrid/css/flexboxgrid.css')
-    .pipe(rename("_flexboxgrid.scss"))
-    .pipe(gulp.dest(srcPath + 'css/'));
-
   gulp.src(srcPath + 'css/**/*')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(destPath + 'css/'))
