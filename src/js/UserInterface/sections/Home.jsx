0218f38a22825ca53dd7123780b81cadafb0d979
Jon Balon <development@jonbalon.com>
2016-08-29 01:01:15 -0400

    Added GitHub, LinkedIn, FCC icons to <Home/>

--- src/js/UserInterface/sections/Home.jsx
+++ src/js/UserInterface/sections/Home.jsx
@@ -29,6 +29,21 @@ export default class Home extends React.Component {
 
         <div className="hr"/>
 
+        <div className="container-icons text-center">
+          <a href="" target="_blank">
+            <i className="icon icon-github"></i>
+          </a>
+
+          <a href="" target="_blank">
+            <i className="icon icon-linkedin"></i>
+          </a>
+
+          <a href="" target="_blank">
+            <i className="icon icon-freecodecamp"></i>
+          </a>
+
+        </div>
+
       </div>
     );
   } // End Component Render.
