bf60df1410c475c37379e118113f59f3452f7452
Jon Balon <development@jonbalon.com>
2016-08-28 22:06:23 -0400

    Added placeholder text to remaining components

--- src/js/UserInterface/sections/Portfolio.jsx
+++ src/js/UserInterface/sections/Portfolio.jsx
@@ -13,7 +13,25 @@ export default class Portfolio extends React.Component {
   render() {
     return (
       <div className="section" id="section-portfolio">
-        Portfolio
+
+        <div className="title">Portfolio Projects</div>
+
+        <p>
+        Commodo nulla Lorem cillum occaecat deserunt consectetur proident commodo officia irure nisi adipisicing. Anim esse culpa ad excepteur deserunt ut tempor nulla quis amet officia minim occaecat sunt sit tempor. Lorem occaecat enim sit consequat pariatur veniam nostrud exercitation proident irure proident ea incididunt. Consequat ullamco ullamco adipisicing velit laborum aliquip velit ipsum ut excepteur. In est sit reprehenderit enim commodo mollit irure pariatur. Ullamco aute ex esse quis occaecat irure culpa aliquip Lorem nostrud magna dolore pariatur. Enim mollit deserunt magna nisi veniam laborum Lorem quis magna esse ea aliqua anim est ex occaecat.
+        </p>
+
+        <p>
+        Nostrud laboris fugiat consectetur cupidatat mollit excepteur incididunt ut mollit deserunt cillum nulla sunt Lorem quis dolore. Incididunt proident nulla minim nisi amet anim anim in Lorem duis. Cillum commodo nostrud aute aute cillum ex occaecat occaecat magna ad quis eu. Culpa eiusmod deserunt reprehenderit Lorem anim reprehenderit fugiat labore ex id. Nulla laboris fugiat cillum consequat deserunt voluptate ea elit sit irure. Consectetur fugiat est ullamco deserunt dolore ea est officia. Eu qui consequat sunt in ullamco elit laborum nisi eiusmod id elit aliquip do enim labore proident nisi.
+        </p>
+
+        <p>
+        Laboris in aliqua ut anim tempor labore tempor amet anim sunt duis do. Consectetur veniam elit ea qui tempor consequat est sint cupidatat laboris voluptate ipsum et excepteur. Ullamco elit aliquip nostrud aute laboris cillum dolor labore in exercitation do duis enim tempor proident incididunt commodo.
+        </p>
+
+        <p>
+        Aute exercitation consequat sint laboris veniam consectetur velit enim enim exercitation ea magna adipisicing. Irure consectetur sunt consectetur velit mollit quis ullamco aliqua quis excepteur incididunt voluptate labore nulla velit labore. Nulla irure ipsum sunt ipsum consequat quis excepteur eu cupidatat elit elit nostrud consequat. Sint nulla commodo incididunt duis velit tempor ullamco qui eiusmod non qui cillum consectetur commodo ut.
+        </p>
+
       </div>
     );
   } // End Component Render.
