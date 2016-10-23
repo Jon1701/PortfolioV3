1bb6b9c70d756aeeec5494ed89bc545e4ee6f2b1
Jon Balon <development@jonbalon.com>
2016-10-23 18:46:06 -0400

    Delete old website

--- src/js/index.jsx
+++
@@ -1,9 +0,0 @@
-// React.
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// Import User Interface.
-import UserInterface from './UserInterface/UserInterface.jsx';
-
-// Render <UserInterface/> to the DOM.
-ReactDOM.render(<UserInterface/>, document.getElementById('react-target'));
