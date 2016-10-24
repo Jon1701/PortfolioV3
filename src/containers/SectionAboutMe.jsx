ef21ebc79a3094e26097268376616d86e95293d3
Jon Balon <development@jonbalon.com>
2016-10-24 01:18:07 -0400

    Fixed unique key warning

--- src/containers/SectionAboutMe.jsx
+++ src/containers/SectionAboutMe.jsx
@@ -27,6 +27,7 @@ export default class SectionAboutMe extends React.Component {
 
       return (
         <Icon
+          key={"icon-aboutme-"+idx}
           iconClass={"icon " + iconData[val]['icon']}
           iconName={iconData[val]['name']}
           containerClasses={"col-xs-4 col-sm-3 col-lg-2"}
