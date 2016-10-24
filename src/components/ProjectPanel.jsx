da71732bffb183fe41cdab4274fd492f3d6e20d7
Jon Balon <development@jonbalon.com>
2016-10-23 22:07:56 -0400

    Started work on ProjectPanel

---
+++ src/components/ProjectPanel.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const ProjectPanel = () => (
+
+  <div>
+    ProjectPanel
+  </div>
+)
+
+export default ProjectPanel;
+
+/*
+
+      const title = project['title'];
+      const imageUrl = require('images/portfolio/' + project['image']);
+      const description = project['description'];
+      const technologies = project['technologies'];
+      const featured = project['featured'];
+
+      const linkGithub = project['links']['github'];
+      const linkDemo = project['links']['demo'];
+
+      return (
+
+        <div className="project-panel col-xs-12 col-sm-6 col-md-6 col-lg-4" key={title}>
+
+          <div className="project-title">{title}</div>
+          <div className="project-image-container">
+            <img src={imageUrl} className="project-image img-responsive"/>
+          </div>
+          <div className="project-buttons">
+
+          </div>
+
+        </div>
+
+      )
+*/
