0c1a20d04fc189b229d1b6d7d6f613c5c0174776
Jon Balon <development@jonbalon.com>
2016-10-26 13:23:07 -0400

    Connected application to redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+// React.
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Redux.
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+// Container containing website UI elements.
 import WebsiteUI from 'containers/WebsiteUI'
 
-ReactDOM.render(<div><WebsiteUI/></div>, document.getElementById('react-target'));
+// Redux reducers and store..
+import reducers from 'reducers/index.js'
+let store = createStore(reducers);
+
+ReactDOM.render(<Provider store={store}><WebsiteUI/></Provider>, document.getElementById('react-target'));
