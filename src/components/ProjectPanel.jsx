86cec4aa6d8081f5bc6a9d5411af1571831ce178
Jon Balon <development@jonbalon.com>
2016-10-27 23:23:03 -0400

    Made component aware of Redux

--- src/components/ProjectPanel.jsx
+++ src/components/ProjectPanel.jsx
@@ -1,19 +1,37 @@
+// React dependencies.
 import React from 'react';
 
+// Redux dependencies.
+import { connect } from 'react-redux';
+
+// React components.
 import Icon from 'components/Icon'
 import MoreInfoPopover from 'components/MoreInfoPopover'
 
-export default class ProjectPanel extends React.Component {
+// Actions.
+import { togglePopover } from 'actions/index.js';
+import { bindActionCreators } from 'redux'
+
+// Component definition.
+class ProjectPanel extends React.Component {
+
+  // Component render.
   render() {
 
+    // Shorthand access to data props.
     const project = this.props.data;
 
+    // Access ID of this project.
+    const projectId = this.props.projectId;
+
+    // Project metadata.
     const title = project['title'];
     const imageUrl = require('images/portfolio/' + project['image']);
     const description = project['description'];
     const technologies = project['technologies'];
     const featured = project['featured'];
 
+    // Project links.
     const linkGithub = project['links']['github'];
     const linkDemo = project['links']['demo'];
 
@@ -34,11 +52,11 @@ export default class ProjectPanel extends React.Component {
 
           <div className="project-buttons custom-row cursor-hand noselect">
             <Icon iconClass={"icon icon-demo"} iconName={"Demo"} link={linkDemo} containerClasses={"icon-hover-crimson col-xs-4 col-sm-4 col-md-4 col-lg-4"}/>
-            <Icon iconClass={"icon icon-moreinfo"} iconName={"More Info"} containerClasses={"icon-hover-crimson col-xs-4 col-sm-4 col-md-4 col-lg-4"}/>
+            <Icon iconClass={"icon icon-moreinfo"} iconName={"More Info"} containerClasses={"icon-hover-crimson col-xs-4 col-sm-4 col-md-4 col-lg-4"} onClick={() => this.props.togglePopover(this.props.projectId)}/>
             <Icon iconClass={"icon icon-github"} iconName={"GitHub"} link={linkGithub} containerClasses={"icon-hover-crimson col-xs-4 col-sm-4 col-md-4 col-lg-4"}/>
           </div>
 
-          <MoreInfoPopover description={description} tech={technologies}/>
+          <MoreInfoPopover description={description} tech={technologies} projectId={projectId}/>
 
         </div>
 
@@ -47,3 +65,15 @@ export default class ProjectPanel extends React.Component {
     )
   }
 }
+
+// Function to allow access of dispatch actions as props.
+//
+// this.props.togglePopover().
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    togglePopover: togglePopover
+  }, dispatch)
+}
+
+// Allows this component to access the Redux store.
+export default connect(null, mapDispatchToProps)(ProjectPanel);
