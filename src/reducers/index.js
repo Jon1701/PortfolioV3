1ceab729ff05f1fbcf7abd16d8a61cf4a4e8a3f8
Jon Balon <development@jonbalon.com>
2016-10-27 23:19:56 -0400

    Added reducer for handling currently active More Info popover

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,16 @@
+// Redux dependencies.
 import { combineReducers } from 'redux';
 
-// Project Data reducer.
-import projectData from './reducer_projectData'
+// Reducers.
+import projectData from './reducer_projectData';
+import activePopoverId from './reducer_activePopoverId';
 
 // Combine all reducers together into one state object.
 const reducers = combineReducers({
-  projectData: projectData
+  projectData: projectData,         // Array of portfolio projects.
+  activePopoverId: activePopoverId, // ID of the project whose More Info popover
+                                    // should be displayed.
 })
 
-// Export the state.
+// Export the reducers.
 export default reducers;
