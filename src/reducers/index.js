dee9885451deee3841ba181028a0518eb5a07f02
Jon Balon <development@jonbalon.com>
2016-10-28 00:55:14 -0400

    Added reducer for toggling display of featured/all projects

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,17 @@ import { combineReducers } from 'redux';
 // Reducers.
 import projectData from './reducer_projectData';
 import activePopoverId from './reducer_activePopoverId';
+import showOnlyFeaturedProjects from './reducer_featuredProjects';
 
 // Combine all reducers together into one state object.
 const reducers = combineReducers({
   projectData: projectData,         // Array of portfolio projects.
+
   activePopoverId: activePopoverId, // ID of the project whose More Info popover
                                     // should be displayed.
+
+  showOnlyFeaturedProjects: showOnlyFeaturedProjects, // Toggle to show only featured
+                                                      // projects or all projects.
 })
 
 // Export the reducers.
