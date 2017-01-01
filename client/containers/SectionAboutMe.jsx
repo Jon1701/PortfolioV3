8d0403cd69de5e08d68daa9eceb8aa204b93bff1
Jon Balon <development@jonbalon.com>
2017-01-01 01:40:26 -0500

    Refactored into stateless component, used airbnb style guide

--- client/containers/SectionAboutMe.jsx
+++ client/containers/SectionAboutMe.jsx
@@ -1,34 +1,41 @@
 // React.
 import React from 'react';
 
-// Load icon data from JSON file.
-const iconData = require('json/icons.json');
-
+// React Components.
 import Icon from 'components/Icon';
 import IconMatrix from 'components/IconMatrix';
 
-// Component definition.
-export default class SectionAboutMe extends React.Component {
+// Other.
+const iconData = require('json/icons.json'); // Icon data.
 
-  // Component render.
-  render() {
-    return (
-      <div id="section-aboutme" className="page-container text-center">
-        <div className="page-content">
+/*
+ *
+ *  Component Definition.
+ *
+ */
+const SectionAboutMe = () => (
+  <div id="section-aboutme" className="page-container text-center">
+    <div className="page-content">
 
-          <div className="page-heading">About Me</div>
-
-          <p>Hi! My name is Jon and I'm a Full Stack Developer based in Toronto.</p>
-
-          <p>Here are some of the technologies I have used:</p>
-
-          <div className="row cursor-arrow">
-            <IconMatrix/>
-          </div>
-
-        </div>
+      <div className="page-heading">
+        About Me
       </div>
-    )
-  } // End Component render.
 
-}
+      <p>
+        Hi! My name is Jon and I'm a Full Stack Developer based in Toronto.
+      </p>
+
+      <p>
+        Here are some of the technologies I have used:
+      </p>
+
+      <div className="row cursor-arrow">
+        <IconMatrix />
+      </div>
+
+    </div>
+  </div>
+);
+
+// Component export.
+export default SectionAboutMe;
