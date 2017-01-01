37ee533566b9e3219f51ffa05d79b437f3e2bd9b
Jon Balon <development@jonbalon.com>
2017-01-01 01:36:25 -0500

    Refactored into stateless component, used airbnb style guide

--- client/containers/SectionHome.jsx
+++ client/containers/SectionHome.jsx
@@ -1,60 +1,58 @@
 // React.
 import React from 'react';
 
-import Icon from 'components/Icon'
+// Components.
+import Icon from 'components/Icon';
 
-// Component definition.
-export default class SectionHome extends React.Component {
+/*
+ *
+ *  Component Definition.
+ *
+ */
+const SectionHome = () => (
+  <div className="force-table viewport-maxheight">
+    <div className="force-table-row">
 
-  // Component render.
-  render() {
-    return (
-      <div className="force-table viewport-maxheight">
-        <div className="force-table-row">
+      <div id="section-home" className="page-container text-center">
+        <div className="page-content">
 
+          <div className="name">Jon Balon</div>
 
-
-
-
-          <div id="section-home" className="page-container text-center">
-            <div className="page-content">
-              <div className="name">Jon Balon</div>
-
-              <div className="position">Full Stack Developer</div>
-
-              <div className="tagline">I'm a Full Stack Developer with a background in Computer Science, Statistics, Analytics, and Data Science.</div>
-
-              <div className="hr"></div>
-
-              <div className="iconArray">
-
-                <Icon
-                  iconClass={"icon-github"}
-                  link={"https://github.com/Jon1701"}
-                  containerClasses={"icon-hover-heavenlyglow"}
-                />
-                <Icon
-                  iconClass={"icon-linkedin"}
-                  link={"https://ca.linkedin.com/in/jonbalon"}
-                  containerClasses={"icon-hover-heavenlyglow"}
-                />
-                <Icon
-                  iconClass={"icon-twitter"}
-                  link={"https://twitter.com/Jon1764"}
-                  containerClasses={"icon-hover-heavenlyglow"}
-                />
-
-              </div>
-            </div>
+          <div className="position">
+            Full Stack Developer
           </div>
 
+          <div className="tagline">
+            I'm a Full Stack Developer with a background in Computer Science,
+            Statistics, Analytics, and Data Science.
+          </div>
 
+          <div className="hr" />
 
+          <div className="iconArray">
+            <Icon
+              iconClass={'icon-github'}
+              link={'https://github.com/Jon1701'}
+              containerClasses={'icon-hover-heavenlyglow'}
+            />
+            <Icon
+              iconClass={'icon-linkedin'}
+              link={'https://ca.linkedin.com/in/jonbalon'}
+              containerClasses={'icon-hover-heavenlyglow'}
+            />
+            <Icon
+              iconClass={'icon-twitter'}
+              link={'https://twitter.com/Jon1764'}
+              containerClasses={'icon-hover-heavenlyglow'}
+            />
 
-
+          </div>
         </div>
       </div>
-    )
-  } // End Component render.
 
-}
+    </div>
+  </div>
+);
+
+// Component export.
+export default SectionHome;
