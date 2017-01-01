61aa4954bd1120544e411d40822d9182e24b12b2
Jon Balon <development@jonbalon.com>
2017-01-01 12:22:58 -0500

    refactor and code cleanup, created presentational components

--- client/components/ContactFormAlertBox.jsx
+++ client/components/ContactFormAlertBox.jsx
@@ -1,55 +1,79 @@
-// React dependencies.
+// React.
 import React from 'react';
+
+// Other dependencies.
 import classNames from 'classnames';
 
 // String constants.
 const constants = require('json/constants.json');
 
-// Component definition.
+/*
+ *
+ *  Component definition.
+ *
+ */
 export default class ContactFormAlertBox extends React.Component {
 
-  getAlertBox(status) {
+  // Component constructor.
+  constructor(props) {
+    super(props);
 
+    // Bind methods to component instance.
+    this.getAlertBox = this.getAlertBox.bind(this);
+  }
+
+  // Method to get the correct Alert Box.
+  getAlertBox(status) {
+    // Different Alert Box components based on Succes, Failure, or Incomplete.
     switch (status) {
       case constants.FAILURE:
-
-        return (
-          <div className="alertbox alertbox-failure">
-            Server error. Message not sent!
-          </div>
-        )
-
+        return <AlertBoxFail />;
       case constants.SUCCESS:
-        return (
-          <div className="alertbox alertbox-success">
-            Message sent!
-          </div>
-        )
-
+        return <AlertBoxSuccess />;
       case constants.INCOMPLETE:
-        return (
-          <div className="alertbox alertbox-incomplete">
-            Please fill out all fields.
-          </div>
-        )
+        return <AlertBoxIncomplete />;
       default:
-        return (
-          <div className="alertbox alertbox-invisible noselect">
-            Invisible
-          </div>
-        )
-
-    }// end switch.
-
+        return <AlertBoxInvisible />;
+    }
   }
 
   // Component render.
   render() {
-
-    // Get the form submission status.
-    const status = this.props.formSubmissionStatus;
-
-    // Return corresponding alertbox.
-    return this.getAlertBox(status)
+    // Return corresponding Alert Box based on the status of form submission.
+    return this.getAlertBox(this.props.formSubmissionStatus);
   }
 }
+
+/*
+ *
+ *  Presentational Components.
+ *
+ */
+
+// Alert Box: Success.
+const AlertBoxSuccess = () => (
+  <div className="alertbox alertbox-success">
+    Message sent!
+  </div>
+);
+
+// Alert Box: Fail.
+const AlertBoxFail = () => (
+  <div className="alertbox alertbox-failure">
+    Server error. Message not sent!
+  </div>
+);
+
+// Alert Box: Incomplete.
+const AlertBoxIncomplete = () => (
+  <div className="alertbox alertbox-incomplete">
+    Please fill out all fields.
+  </div>
+);
+
+// Alert Box: Invisible.
+const AlertBoxInvisible = () => (
+  <div className="alertbox alertbox-invisible noselect">
+    Invisible
+  </div>
+);
