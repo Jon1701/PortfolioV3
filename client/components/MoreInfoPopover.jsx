4685963f5f991020b5e221c5f86c53be14072dd1
Jon Balon <development@jonbalon.com>
2017-01-01 13:01:42 -0500

    Refactor, helper function moved into render(), added prop validation

--- client/components/MoreInfoPopover.jsx
+++ client/components/MoreInfoPopover.jsx
@@ -8,64 +8,63 @@ import { connect } from 'react-redux';
 // React components.
 import Icon from 'components/Icon'
 
+// Data.
+const iconData = require('json/icons.json');  // Load icon data from JSON file.
+
 // Component definition.
 class MoreInfoPopover extends React.Component {
 
-  buildIcons() {
+  render() {
+    // Deconstruct props.
+    const { tech, activePopoverId, projectId, description } = this.props;
 
-    // Load icon data from JSON file.
-    const iconData = require('json/icons.json');
-
-    const tech = this.props.tech;
-
-    return tech.map((val, idx, arr) => {
-
+    // Build DOM node for project icons.
+    const projectIcons = tech.map((val, idx) => {
+      // Get icon css class and icon name.
       const iconClass = iconData[val]['icon'];
       const iconName = iconData[val]['name'];
 
       return (
         <Icon
-          key={ idx }
-          iconClass={ iconClass }
-          iconName={ iconName }
-          containerClasses={ iconClass + "-hover"}
-          />
-      )
+          key={idx}
+          iconClass={iconClass}
+          iconName={iconName}
+          containerClasses={`${iconClass}-hover`}
+        />
+      );
+    });
 
-    })
-
-  }
-
-  render() {
-
+    // Classes to control visibility of Project Panel Popover.
     const myClasses = classNames({
       'project-popover': true,
       'bg-white': true,
-      'hidden': this.props.activePopoverId != this.props.projectId
-    })
+      hidden: activePopoverId !== projectId,
+    });
 
     return (
       <div className={myClasses}>
-
-        <div className="project-description">{this.props.description}</div>
+        <div className="project-description">{description}</div>
 
         <div className="project-tech">
-          { this.buildIcons() }
+          { projectIcons }
         </div>
-
       </div>
-    )
+    );
   }
 }
 
-// Function to map state to props.
-//
-// this.state.activePopoverId -> this.props.activePopoverId.
-const mapStateToProps = (state) => {
-  return {
-    activePopoverId: state.activePopoverId
-  }
-}
+// Maps state to props.
+const mapStateToProps = state => ({
+  activePopoverId: state.activePopoverId,
+});
 
-// Allows this component to access the Redux store.
-export default connect(mapStateToProps)(MoreInfoPopover);
+// Prop validation.
+MoreInfoPopover.propTypes = {
+  tech: React.PropTypes.arrayOf(React.PropTypes.string),
+  activePopoverId: React.PropTypes.number,
+  projectId: React.PropTypes.number,
+  description: React.PropTypes.string,
+};
+
+// Allow access to redux store.
+export default connect(mapStateToProps, null)(MoreInfoPopover);
