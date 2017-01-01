7a9cf7ccdd8803b3ee5962b1c78f455f9f64b730
Jon Balon <development@jonbalon.com>
2017-01-01 11:45:34 -0500

    Now uses new classes

--- client/containers/SectionProjects.jsx
+++ client/containers/SectionProjects.jsx
@@ -9,22 +9,24 @@ import ProjectPanelMatrix from 'components/ProjectPanelMatrix';
 
 // Component definition.
 const SectionProjects = () => (
-  <div id="section-projects" className="section height-min-100vh text-center">
+  <div id="section-projects" className="page-container text-center viewport-fullpage">
+    <div className="page-content">
 
-    <div className="text-uppercase section-main-heading">
-      Portfolio
+      <div className="page-heading">
+        Portfolio
+      </div>
+
+      <p>
+        Here's some of my work:
+      </p>
+
+      <ProjectPanelMatrix />
+
+      <p>
+        with more to come in the future!
+      </p>
+
     </div>
-
-    <p>
-      Here's some of my work:
-    </p>
-
-    <ProjectPanelMatrix />
-
-    <p>
-      with more to come in the future!
-    </p>
-
   </div>
 );
 
