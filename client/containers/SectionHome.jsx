a36cd5d433fa630b05b6d479528335b11a66a2a2
Jon Balon <development@jonbalon.com>
2017-01-01 01:09:16 -0500

    Added screen max width

--- client/containers/SectionHome.jsx
+++ client/containers/SectionHome.jsx
@@ -9,43 +9,43 @@ export default class SectionHome extends React.Component {
   // Component render.
   render() {
     return (
-      <div className="force-table height-minmax-100vh">
+      <div className="force-table viewport-maxheight">
         <div className="force-table-row">
 
 
 
 
 
-          <div id="section-home" className=" text-center">
+          <div id="section-home" className="page-container text-center">
+            <div className="page-content">
+              <div className="name">Jon Balon</div>
 
-            <div className="text-center name">Jon Balon</div>
+              <div className="position">Full Stack Developer</div>
 
-            <div className="text-center position">Full Stack Developer</div>
+              <div className="tagline">I'm a Full Stack Developer with a background in Computer Science, Statistics, Analytics, and Data Science.</div>
 
-            <div className="text-center tagline">I'm a Full Stack Developer with a background in Computer Science, Statistics, Analytics, and Data Science.</div>
+              <div className="hr"></div>
 
-            <div className="hr"></div>
+              <div className="iconArray">
 
-            <div className="text-center iconArray">
+                <Icon
+                  iconClass={"icon-github"}
+                  link={"https://github.com/Jon1701"}
+                  containerClasses={"icon-hover-heavenlyglow"}
+                />
+                <Icon
+                  iconClass={"icon-linkedin"}
+                  link={"https://ca.linkedin.com/in/jonbalon"}
+                  containerClasses={"icon-hover-heavenlyglow"}
+                />
+                <Icon
+                  iconClass={"icon-twitter"}
+                  link={"https://twitter.com/Jon1764"}
+                  containerClasses={"icon-hover-heavenlyglow"}
+                />
 
-              <Icon
-                iconClass={"icon-github"}
-                link={"https://github.com/Jon1701"}
-                containerClasses={"icon-hover-heavenlyglow"}
-              />
-              <Icon
-                iconClass={"icon-linkedin"}
-                link={"https://ca.linkedin.com/in/jonbalon"}
-                containerClasses={"icon-hover-heavenlyglow"}
-              />
-              <Icon
-                iconClass={"icon-twitter"}
-                link={"https://twitter.com/Jon1764"}
-                containerClasses={"icon-hover-heavenlyglow"}
-              />
-
+              </div>
             </div>
-
           </div>
 
 
