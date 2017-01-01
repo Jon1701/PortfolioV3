2d9330ccc77ccfb99b804b46861dc66b486a4c5f
Jon Balon <development@jonbalon.com>
2017-01-01 12:05:57 -0500

    Added proptypes, refactor, code cleanup, followed airbnb style guide

--- client/components/ContactForm.jsx
+++ client/components/ContactForm.jsx
@@ -1,13 +1,12 @@
-// React dependencies.
+// React.
 import React from 'react';
-import classNames from 'classnames';
+
+// Components.
+import ContactFormAlertBox from 'components/ContactFormAlertBox'
 
 // Redux dependencies.
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-
-// Components.
-import ContactFormAlertBox from 'components/ContactFormAlertBox'
 
 // Actions.
 import { resetForm } from 'actions/index.js';
@@ -17,12 +16,26 @@ import { updateNameField, updateEmailField, updateSubjectField, updateGotchaFiel
 
 // Other libraries.
 import axios from 'axios';  // Requests.
+import classNames from 'classnames';  // Toggleable classes.
 
 // Component definition.
 class ContactForm extends React.Component {
 
+  // Component constructor.
+  constructor(props) {
+    super(props);
+
+    // Bind methods to component instance.
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   // Handles form submission.
   handleSubmit(e) {
+    // Deconstruct props: form field validity.
+    const { nameIsValid, emailIsValid, subjectIsValid, gotchaIsValid, messageIsValid } = this.props;
+
+    // Deconstruct props: form fields.
+    const { name, email, subject, gotcha, message } = this.props;
 
     // Reset form submit status.
     this.props.resetFormSubmissionStatus();
@@ -31,212 +44,220 @@ class ContactForm extends React.Component {
     e.preventDefault();
 
     // Check if all fields are valid.
-    const allFieldsValid = this.props.nameIsValid && this.props.emailIsValid && this.props.subjectIsValid && this.props.gotchaIsValid && this.props.messageIsValid;
+    const allFieldsValid = nameIsValid && emailIsValid && subjectIsValid
+      && gotchaIsValid && messageIsValid;
 
     // Send AJAX request if all fields are valid.
     if (allFieldsValid) {
-
       // Disable the submit button.
       this.props.disableSubmitButton();
 
       // Submit form.
       axios({
         method: 'post',
-        //url: 'https://formspree.io/testyouremail@mailinator.com',
         url: atob('aHR0cHM6Ly9mb3Jtc3ByZWUuaW8vam9uLmouYmFsb25AZ21haWwuY29t'),
         data: {
-          name: this.props.name,
-          email: this.props.email,
-          _subject: this.props.subject,
-          _gotcha: this.props.gotcha,
-          message: this.props.message
-        }
-      }).then((res) => {
-
+          name,
+          email,
+          message,
+          _subject: subject,
+          _gotcha: gotcha,
+        },
+      }).then(() => {
         // Update form submit status to success.
         this.props.setFormSubmissionStatusToSuccess();
 
         // Reset form after 5 seconds.
         setTimeout(this.props.resetForm, 5000);
-
-      }).catch((err) => {
-
+      }).catch(() => {
         // Enable Submit button.
         this.props.enableSubmitButton();
 
         // Update form status to show indicator to user.
         this.props.setFormSubmissionStatusToFailure();
-
-      })
-
+      });
     } else {
-
       // Update form submit status to incomplete.
       this.props.setFormSubmissionStatusToIncomplete();
-
     }
-
   }
 
   // Component render.
   render() {
+    // Deconstruct props: form field validity.
+    const { nameIsValid, emailIsValid, subjectIsValid, messageIsValid } = this.props;
 
-    // CSS classes to indicate to the user if the Name field
-    // is valid or not.
+    // Deconstruct props: form fields.
+    const { name, email, subject, gotcha, message } = this.props;
+
+    // CSS classes to indicate if the Name field is valid or not.
     const nameFieldClasses = classNames({
       'field-validation-default': true,
-      'field-validation-fail': this.props.nameIsValid == false,
-      'field-validation-success': this.props.nameIsValid == true
+      'field-validation-fail': nameIsValid === false,
+      'field-validation-success': nameIsValid === true,
     });
 
-    // CSS classes to indicate to the user if the Email field
-    // is valid or not.
+    // CSS classes to indicate if the Email field is valid or not.
     const emailFieldClasses = classNames({
       'field-validation-default': true,
-      'field-validation-fail': this.props.emailIsValid == false,
-      'field-validation-success': this.props.emailIsValid == true
+      'field-validation-fail': emailIsValid === false,
+      'field-validation-success': emailIsValid === true,
     });
 
-    // CSS classes to indicate to the user if the Subject field
-    // is valid or not.
+    // CSS classes to indicate if the Subject field is valid or not.
     const subjectFieldClasses = classNames({
       'field-validation-default': true,
-      'field-validation-fail': this.props.subjectIsValid == false,
-      'field-validation-success': this.props.subjectIsValid == true
+      'field-validation-fail': subjectIsValid === false,
+      'field-validation-success': subjectIsValid === true,
     });
 
-    // CSS classes to indicate to the user if the Message field
-    // is valid or not.
+    // CSS classes to indicate if the Message field is valid or not.
     const messageFieldClasses = classNames({
       'field-validation-default': true,
-      'field-validation-fail': this.props.messageIsValid == false,
-      'field-validation-success': this.props.messageIsValid == true
+      'field-validation-fail': messageIsValid === false,
+      'field-validation-success': messageIsValid === true,
     });
 
     return (
       <div>
+        <ContactFormAlertBox formSubmissionStatus={this.props.formSubmissionStatus} />
 
-        <ContactFormAlertBox formSubmissionStatus={this.props.formSubmissionStatus}/>
-
-        <form onSubmit={this.handleSubmit.bind(this)}>
+        <form onSubmit={this.handleSubmit}>
 
           <div className="form-field">
             <input
               className={nameFieldClasses}
-              value={this.props.name}
+              value={name}
               name="name"
               type="text"
               placeholder="Name"
-              onChange={ (e) => {this.props.updateNameField(e.target.value)} }
+              onChange={(e) => { this.props.updateNameField(e.target.value); }}
             />
           </div>
 
           <div className="form-field">
             <input
               className={emailFieldClasses}
-              value={this.props.email}
+              value={email}
               name="email"
               type="text"
               placeholder="E-mail address"
-              onChange={ (e) => {this.props.updateEmailField(e.target.value)} }
+              onChange={(e) => { this.props.updateEmailField(e.target.value); }}
             />
           </div>
 
           <div className="form-field">
             <input
               className={subjectFieldClasses}
-              value={this.props.subject}
+              value={subject}
               name="subject"
               type="text"
               placeholder="Subject"
-              onChange={ (e) => {this.props.updateSubjectField(e.target.value)} }
+              onChange={(e) => { this.props.updateSubjectField(e.target.value); }}
             />
           </div>
 
           <div className="form-field hidden">
             <input
-              value={this.props.gotcha}
+              value={gotcha}
               name="_gotcha"
               type="text"
               placeholder="Gotcha"
-              onChange={ (e) => {this.props.updateGotchaField(e.target.value)} }
+              onChange={(e) => { this.props.updateGotchaField(e.target.value); }}
             />
           </div>
 
           <div className="form-field">
             <textarea
               className={messageFieldClasses}
-              value={this.props.message}
+              value={message}
               name="message"
               type="text"
               placeholder="Message"
-              onChange={ (e) => {this.props.updateMessageField(e.target.value)} }
+              onChange={(e) => { this.props.updateMessageField(e.target.value); }}
             />
           </div>
 
           <div className="form-submit-container">
             <button type="submit" disabled={!this.props.statusSubmitButton}>
-              <span className="icon icon-paperplane"/> Send Message
+              <span className="icon icon-paperplane" /> Send Message
             </button>
           </div>
 
         </form>
       </div>
-    )
+    );
   }
 
 }
 
-// Function to map state to props.
-//
-// this.state.contactForm.name -> this.props.name
-// this.state.contactForm.email -> this.props.email
-// this.state.contactForm.nameIsValid -> this.props.nameIsValid
-//
-// and so on...
-const mapStateToProps = (state) => {
-  return {
-    name: state.contactForm.name,
-    email: state.contactForm.email,
-    subject: state.contactForm.subject,
-    gotcha: state.contactForm.gotcha,
-    message: state.contactForm.message,
+// Map state to props.
+const mapStateToProps = state => ({
+  name: state.contactForm.name,
+  email: state.contactForm.email,
+  subject: state.contactForm.subject,
+  gotcha: state.contactForm.gotcha,
+  message: state.contactForm.message,
 
-    nameIsValid: state.contactForm.nameIsValid,
-    emailIsValid: state.contactForm.emailIsValid,
-    subjectIsValid: state.contactForm.subjectIsValid,
-    gotchaIsValid: state.contactForm.gotchaIsValid,
-    messageIsValid: state.contactForm.messageIsValid,
+  nameIsValid: state.contactForm.nameIsValid,
+  emailIsValid: state.contactForm.emailIsValid,
+  subjectIsValid: state.contactForm.subjectIsValid,
+  gotchaIsValid: state.contactForm.gotchaIsValid,
+  messageIsValid: state.contactForm.messageIsValid,
 
-    statusSubmitButton: state.contactForm.statusSubmitButton,
+  statusSubmitButton: state.contactForm.statusSubmitButton,
 
-    formSubmissionStatus: state.contactForm.formSubmissionStatus,
-  }
-}
+  formSubmissionStatus: state.contactForm.formSubmissionStatus,
+});
 
-// Function to allow access of dispatch actions as props.
-//
-// this.props.updateNameField().
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    updateNameField: updateNameField,
-    updateEmailField: updateEmailField,
-    updateSubjectField: updateSubjectField,
-    updateGotchaField: updateGotchaField,
-    updateMessageField: updateMessageField,
+// Maps actions to props.
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    updateNameField,
+    updateEmailField,
+    updateSubjectField,
+    updateGotchaField,
+    updateMessageField,
 
-    resetForm: resetForm,
+    resetForm,
 
-    enableSubmitButton: enableSubmitButton,
-    disableSubmitButton: disableSubmitButton,
+    enableSubmitButton,
+    disableSubmitButton,
 
-    resetFormSubmissionStatus: resetFormSubmissionStatus,
-    setFormSubmissionStatusToSuccess: setFormSubmissionStatusToSuccess,
-    setFormSubmissionStatusToFailure: setFormSubmissionStatusToFailure,
-    setFormSubmissionStatusToIncomplete: setFormSubmissionStatusToIncomplete,
+    resetFormSubmissionStatus,
+    setFormSubmissionStatusToSuccess,
+    setFormSubmissionStatusToFailure,
+    setFormSubmissionStatusToIncomplete,
   }, dispatch)
-}
-
+);
 
 // Allows this component to access the Redux store.
 export default connect(mapStateToProps, mapDispatchToProps)(ContactForm);
+
+// Prop validation.
+ContactForm.propTypes = {
+  nameIsValid: React.PropTypes.bool,
+  emailIsValid: React.PropTypes.bool,
+  subjectIsValid: React.PropTypes.bool,
+  gotchaIsValid: React.PropTypes.bool,
+  messageIsValid: React.PropTypes.bool,
+  name: React.PropTypes.string,
+  email: React.PropTypes.string,
+  subject: React.PropTypes.string,
+  gotcha: React.PropTypes.string,
+  message: React.PropTypes.string,
+  formSubmissionStatus: React.PropTypes.bool,
+  updateNameField: React.PropTypes.func,
+  updateEmailField: React.PropTypes.func,
+  updateSubjectField: React.PropTypes.func,
+  updateGotchaField: React.PropTypes.func,
+  updateMessageField: React.PropTypes.func,
+  resetForm: React.PropTypes.func,
+  enableSubmitButton: React.PropTypes.func,
+  disableSubmitButton: React.PropTypes.func,
+  resetFormSubmissionStatus: React.PropTypes.func,
+  setFormSubmissionStatusToSuccess: React.PropTypes.func,
+  setFormSubmissionStatusToFailure: React.PropTypes.func,
+  setFormSubmissionStatusToIncomplete: React.PropTypes.func,
+  statusSubmitButton: React.PropTypes.func,
+};
