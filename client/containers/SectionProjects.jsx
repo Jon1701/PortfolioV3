5db8d804c5bd739aa1d8620e2293f3bdc24f532d
Jon Balon <development@jonbalon.com>
2017-02-13 19:40:32 -0500

    Remove ProjectPanelMatrix

--- client/containers/SectionProjects.jsx
+++ client/containers/SectionProjects.jsx
@@ -4,9 +4,6 @@ import React from 'react';
 // Redux dependencies.
 import { connect } from 'react-redux';
 
-// Presentational components.
-import ProjectPanelMatrix from 'components/ProjectPanelMatrix';
-
 // Component definition.
 const SectionProjects = () => (
   <div id="section-projects" className="page-container text-center viewport-fullpage">
@@ -20,7 +17,7 @@ const SectionProjects = () => (
         Here's some of my work:
       </p>
 
-      <ProjectPanelMatrix />
+
 
       <p>
         with more to come in the future!
