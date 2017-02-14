c6b8f0dea2d919fcd24bcf9725f240f8fc39b166
Jon Balon <development@jonbalon.com>
2017-02-13 19:38:22 -0500

    Removed extra double quote

--- client/components/Icon2.jsx
+++ client/components/Icon2.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 // Component definition.
 const Icon = ({ link, iconClass, label, className }) => (
-  <div className={`"container-icon ${className}`}>
+  <div className={`container-icon ${className}`}>
     <a href={link} target="_blank" rel="noopener noreferrer">
       <div className={`icon ${iconClass}`} />
       <div className="label">{label}</div>
