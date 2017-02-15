494a6aeb618f36a839014906387541e2a6896876
Jon Balon <development@jonbalon.com>
2017-02-15 10:59:57 -0500

    Added new <ProjectPanel/>, new CSS

--- client/containers/SectionProjects.jsx
+++ client/containers/SectionProjects.jsx
@@ -1,11 +1,26 @@
 // React.
 import React from 'react';
 
+// React Components.
+import ProjectPanel from 'components/ProjectPanel';
+
+/*
+ *  Helper function to generate <ProjectPanel /> elements.
+ */
+const generateProjectPanels = () => {
+  // Load project data.
+  const projectData = require('json/projects.json');
+
+  // Generate <ProjectPanel /> elements.
+  return projectData.map((project, idx) => (
+    <ProjectPanel key={`project-panel-${idx}`} projectData={project} />
+  ));
+};
+
 // Component definition.
 const SectionProjects = () => (
   <div id="section-projects" className="viewport-fullpage">
     <div className="container-section-content text-center">
-
       <div className="heading">
         Portfolio
       </div>
@@ -14,10 +29,13 @@ const SectionProjects = () => (
         Here's some of my work:
       </p>
 
+      <div className="container-projects row">
+        {generateProjectPanels()}
+      </div>
+
       <p>
         with more to come in the future!
       </p>
-
     </div>
   </div>
 );
