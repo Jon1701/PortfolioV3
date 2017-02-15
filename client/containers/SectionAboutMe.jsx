60a7a295361e5bfb27fa530581e728f774ebd990
Jon Balon <development@jonbalon.com>
2017-02-15 00:14:14 -0500

    Added content margins, font sizes

--- client/containers/SectionAboutMe.jsx
+++ client/containers/SectionAboutMe.jsx
@@ -24,7 +24,7 @@ const generateIcons = () => {
     const iconClass = iconData[name].icon;  // Icon class name.
     const iconLabel = iconData[name].label; // Icon label.
     const iconHoverClass = `${iconData[name].icon}-hover`;  // Icon hover class name.
-    const className = `col-xs-4 col-sm-3 col-md-2 col-lg-1 cursor-hand ${iconHoverClass}`;  // Classes.
+    const className = `col-xs-4 col-sm-3 col-md-2 col-lg-2 cursor-hand ${iconHoverClass}`;  // Classes.
 
     // Return an <Icon/>.
     return (
@@ -42,7 +42,7 @@ const SectionAboutMe = () => (
   <div id="section-aboutme" className="viewport-fullpage">
     <div className="container-section-content text-center">
 
-      <div>
+      <div className="heading">
         About Me
       </div>
 
