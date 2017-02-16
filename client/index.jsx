d7832f33338ab9bfe9e7b1323e92e9c48c47cccf
Jon Balon <development@jonbalon.com>
2017-02-16 09:36:57 -0500

    Removed redux actions, reducers, createStore, and Provider, libraries will be removed at a later date

--- client/index.jsx
+++ client/index.jsx
@@ -2,15 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Redux.
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
 // Container containing website UI elements.
 import WebsiteUI from 'containers/WebsiteUI';
 
-// Redux reducers and store..
-import reducers from 'reducers/index.js';
-const store = createStore(reducers);
-
-ReactDOM.render(<Provider store={store}><WebsiteUI/></Provider>, document.getElementById('react-target'));
+ReactDOM.render(<WebsiteUI/>, document.getElementById('react-target'));
